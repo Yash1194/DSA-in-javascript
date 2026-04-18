233abce2c832dd0849e008b1d8fc66a2ede6031f
Yash Swarnkar <ys703981@gmail.com>
2026-04-19 02:34:35 +0530

    X star pattern

---
+++ day-16/X-shape.js
@@ -0,0 +1,17 @@
+let prompt = require("prompt-sync")();
+
+let nStr = prompt('Enter the number of rows for the X shape pattern: ');
+let n = Number(nStr);
+function printXShapePattern(n) {
+    for(let i =1; i<=n; i++){
+        for(let j=1; j<=n; j++){
+            if(i==j || i+j == n+1){
+                process.stdout.write("*")
+            }else{
+                process.stdout.write(" ")
+            }
+        }console.log();
+
+    }
+}
+printXShapePattern(n);
